00b482d10488251a19e15d51d9837097181d04b5
herugatti <gabrielmcnaught@gmail.com>
2024-11-26 13:25:08 -0500

    Calculator
    Calculator Project for Mr. Jean

---
+++ calculator.py
@@ -0,0 +1,14 @@
+x=5
+y=9
+
+Addition = x+y
+Subtraction = y-x
+Multipilcation = x*y
+Division = x/y
+Power = x**y
+
+print(Addition)
+print(Subtraction)
+print(Multipilcation)
+print(Division)
+print(Power)
